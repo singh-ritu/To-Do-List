e79559aa08788e7d2af596c5d5344b92bb7a8cd5
Shashank Gaur <gaurshashank014@gmail.com>
2023-05-05 12:22:26 +0530

    added function call on enter press

--- todo.js
+++ todo.js
@@ -15,4 +15,15 @@ function handleAddTodo() {
   list.appendChild(divElement);
   input.value = "";
 }
-// list > divElement > h2Element
+
+// how to trigger click event in javascript
+const button = document.querySelector(".btn");
+
+document.addEventListener("keyup", function (e) {
+  if (e.key == "Enter" && input.value !== "") {
+    console.log("enter is clicked");
+    handleAddTodo();
+  }
+});
+
+button.addEventListener("click", handleAddTodo);
